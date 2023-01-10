d58fface7bf2eff7f85c68301641b9ad07ceb74e
Luca Palano <1812530+lucapalano@users.noreply.github.com>
2023-01-10 21:56:29 +0100

    Add mk file allowing to use coreJSON like a component in esp8266 projects

---
+++ component.mk
@@ -0,0 +1,7 @@
+#
+# "json-parser" pseudo-component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+COMPONENT_SRCDIRS         := source
+COMPONENT_ADD_INCLUDEDIRS := source/include
